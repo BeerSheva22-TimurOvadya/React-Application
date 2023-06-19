2f6021aae87697b23f445d7f7c5a92c3657c1ad7
Ovadya33 <timurgts2710@gmail.com>
2023-06-19 21:19:23 +0300

    home work method Next

--- src/service/LifeMatrix.ts
+++ src/service/LifeMatrix.ts
@@ -6,7 +6,34 @@ export default class LifeMatrix {
         return this._numbers;
     }
     next(): number[][] {
-        this._numbers = getRandomMatrix(this._numbers.length, this._numbers[0].length, 0, 2);
+        this._numbers = this._numbers.map((row, i) =>
+            row.map((cell, j) => {
+                const aliveNeighbours = this.countAliveNeighbours(i, j);
+                if (cell === 1) {
+                    return aliveNeighbours === 2 || aliveNeighbours === 3 ? 1 : 0;
+                } else {
+                    return aliveNeighbours === 3 ? 1 : 0;
+                }
+            }),
+        );
         return this._numbers;
+    }
+
+    private countAliveNeighbours(i: number, j: number): number {
+        const rowIndices = [i - 1, i, i + 1];
+        const colIndices = [j - 1, j, j + 1];
+
+        const neighbours = rowIndices.flatMap((ri) =>
+            colIndices.map((ci) => (ri === i && ci === j ? 0 : this.getCell(ri, ci))),
+        );
+
+        return neighbours.filter(Boolean).length;
+    }
+
+    private getCell(i: number, j: number): number {
+        if (i >= 0 && i < this._numbers.length && j >= 0 && j < this._numbers[0].length) {
+            return this._numbers[i][j];
+        }
+        return 0;
     }
 }
