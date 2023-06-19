11d194242f6099a8f337968e19a8924898a74f11
Ovadya33 <timurgts2710@gmail.com>
2023-06-19 21:18:35 +0300

    classWork LifeGame

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import { ReactNode, useState } from 'react';
-import Clocks from './components/Clocks';
-import Input from './components/common/Input';
-import InputResult from './model/InputResult';
-import A from './components/A';
+import LifeGame from './components/LifeGame';
 
-const components: Map<string, ReactNode> = new Map([
-    ['clocks', <Clocks></Clocks>],
-    ['a', <A></A>],
-]);
 const App: React.FC = () => {
-    const [componentName, setComponentName] = useState<string>('');
-    function submitFn(component: string): InputResult {
-        const res: InputResult = { status: 'error', message: `${component} doesn't exist` };
-        if(components.has(component)){
-            res.status = "success";
-            res.message = '';
-            setComponentName(component);
-        }
-        return res;
-    }
     return (
         <div>
-            <Input submitFn={submitFn} placeholder="Enter component name"></Input>
-            {componentName && components.get(componentName)}
+            <LifeGame /> <br></br> <br></br> <LifeGame />
         </div>
     );
 };
