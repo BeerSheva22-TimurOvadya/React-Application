e2f94f74f126b2e1a5cab363f80c58b5fd09ccf5
Ovadya33 <timurgts2710@gmail.com>
2023-06-22 14:33:38 +0300

    classWork clear

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import { matrixSum, range } from './util/number-functions';
-import LifeMatrix from './service/LifeMatrix';
+
 
 test('sum of matrix', () => {
     expect(
@@ -18,38 +18,4 @@ test('range test', () => {
 });
 
 
-test('2x2 square stays static', () => {
-    const initialMatrix = [
-        [0, 0, 0, 0],
-        [0, 1, 1, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 0]
-    ];
-    const lifeMatrix = new LifeMatrix(initialMatrix);
 
-    expect(lifeMatrix.next()).toEqual(initialMatrix);
-    expect(lifeMatrix.next()).toEqual(initialMatrix);
-});
-
-
-test('vertical 3x1 stick flips to horizontal and back', () => {
-    const verticalStick = [
-      [0, 0, 0, 0, 0],
-      [0, 0, 1, 0, 0],
-      [0, 0, 1, 0, 0],
-      [0, 0, 1, 0, 0],
-      [0, 0, 0, 0, 0]
-    ];
-
-    const horizontalStick = [
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 1, 1, 1, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0]
-    ];
-    const lifeMatrix = new LifeMatrix(verticalStick);
-
-    expect(lifeMatrix.next()).toEqual(horizontalStick);
-    expect(lifeMatrix.next()).toEqual(verticalStick);
-});
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import { useEffect } from 'react';
-import LifeGame from './components/LifeGame';
-import { useDispatch } from 'react-redux';
-import { sizeActions } from './redux/slices/cellSizeSlice';
-import { directionActions } from './redux/slices/flexDirectionSlice';
-import Lifes from './components/Lifes';
+
 
 const App: React.FC = () => {
-    const dispatch = useDispatch<any>();
-    useEffect(() => {
-        window.addEventListener('resize', () => {
-            dispatch(sizeActions.setSize());
-            dispatch(directionActions.setDirection());
-        });
-    }, []);
+    
     return (
         <div>
-            <Lifes />
+          
         </div>
     );
 };
