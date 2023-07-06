887dc7255717fe499b66c9bbde56b01e8743c4e1
Ovadya33 <timurgts2710@gmail.com>
2023-07-06 11:57:19 +0300

    classwork

--- src/components/pages/AddEmployee.tsx
+++ src/components/pages/AddEmployee.tsx
@@ -1,34 +1,32 @@
-import Employee from "../../model/Employee";
-import { EmployeeForm } from "../forms/EmployeeForm";
-import InputResult from "../../model/InputResult";
-import { authService, employeesService } from "../../config/service-config";
-import { useDispatch } from "react-redux";
-import { authActions } from "../../redux/slices/authSlice";
-import CodePayload from "../../model/CodePayload";
-import CodeType from "../../model/CodeType";
-import { codeActions } from "../../redux/slices/codeSlice";
+import Employee from '../../model/Employee';
+import { EmployeeForm } from '../forms/EmployeeForm';
+import InputResult from '../../model/InputResult';
+import { employeesService } from '../../config/service-config';
+import { useDispatch } from 'react-redux';
+import CodePayload from '../../model/CodePayload';
+import CodeType from '../../model/CodeType';
+import { codeActions } from '../../redux/slices/codeSlice';
 
 const AddEmployee: React.FC = () => {
     const dispatch = useDispatch();
-    const codeMessage: CodePayload = {code: CodeType.OK, message: ''};
+    const codeMessage: CodePayload = { code: CodeType.OK, message: '' };
     async function submitFn(empl: Employee): Promise<InputResult> {
-        const res: InputResult = {status: 'success', message: ''};
+        const res: InputResult = { status: 'success', message: '' };
         try {
             const employee: Employee = await employeesService.addEmployee(empl);
-            codeMessage.message = `employee with id: ${employee.id} has been added`
+            codeMessage.message = `employee with id: ${employee.id} has been added`;
         } catch (error: any) {
-           res.status = 'error' ;
+            res.status = 'error';
 
-           if( error.includes('Authentication')) {
-            codeMessage.code = CodeType.AUTH_ERROR;
-            codeMessage.message = ''
-            
-           } 
-           codeMessage.message = error;
+            if (error.includes('Authentication')) {
+                codeMessage.code = CodeType.AUTH_ERROR;
+                codeMessage.message = '';
+            }
+            codeMessage.message = error;
         }
-        dispatch(codeActions.set(codeMessage))
+        dispatch(codeActions.set(codeMessage));
         return res;
     }
-    return <EmployeeForm submitFn={submitFn}/>
-}
+    return <EmployeeForm submitFn={submitFn} />;
+};
 export default AddEmployee;
