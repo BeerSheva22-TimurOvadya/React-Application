101eab48ceeb35e7566d0bc063ac69d4cbe1c70d
Ovadya33 <timurgts2710@gmail.com>
2023-08-20 14:33:29 +0300

    123

--- src/config/service-config.ts
+++ src/config/service-config.ts
@@ -9,5 +9,5 @@ import EmployeesServiceRest from '../service/crud/EmployeesServiceRest';
 export const authService: AuthService = new AuthServiceJwt('http://localhost:3500/login'); 
 
 export const employeesService: EmployeesService = new EmployeesServiceRest(
-    'http://localhost:3500/employees',
+    'localhost:3500',
 ); 
--- src/service/crud/EmployeesServiceRest.ts
+++ src/service/crud/EmployeesServiceRest.ts
@@ -2,25 +2,8 @@ import { Observable, Subscriber } from 'rxjs';
 import Employee from '../../model/Employee';
 import { AUTH_DATA_JWT } from '../auth/AuthServiceJwt';
 import EmployeesService from './EmployeesService';
-const POLLER_INTERVAL = 30000;
-class Cache {
-    cacheString: string = '';
-    set(employees: Employee[]): void {
-        this.cacheString = JSON.stringify(employees);
-    }
-    reset() {
-        this.cacheString = ''
-    }
-    isEqual(employees: Employee[]): boolean {
-        return this.cacheString === JSON.stringify(employees)
-    }
-    getCache(): Employee[] {
-        return !this.isEmpty() ? JSON.parse(this.cacheString) : []
-    }
-    isEmpty(): boolean {
-        return this.cacheString.length === 0;
-    }
-}
+import { CompatClient, Stomp } from '@stomp/stompjs';
+const TOPIC = '/topic/employees';
 async function getResponseText(response: Response): Promise<string> {
     let res = '';
     if (!response.ok) {
@@ -28,13 +11,12 @@ async function getResponseText(response: Response): Promise<string> {
         res = status == 401 || status == 403 ? 'Authentication' : await response.text();
     }
     return res;
-
 }
 function getHeaders(): HeadersInit {
     const res: HeadersInit = {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem(AUTH_DATA_JWT) || ''}`
-    }
+        Authorization: `Bearer ${localStorage.getItem(AUTH_DATA_JWT) || ''}`,
+    };
     return res;
 }
 async function fetchRequest(url: string, options: RequestInit, empl?: Employee): Promise<Response> {
@@ -46,9 +28,9 @@ async function fetchRequest(url: string, options: RequestInit, empl?: Employee):
     let flUpdate = true;
     let responseText = '';
     try {
-        if (options.method == "DELETE" || options.method == "PUT") {
+        if (options.method == 'DELETE' || options.method == 'PUT') {
             flUpdate = false;
-            await fetchRequest(url, {method: "GET"});
+            await fetchRequest(url, { method: 'GET' });
             flUpdate = true;
         }
 
@@ -62,70 +44,81 @@ async function fetchRequest(url: string, options: RequestInit, empl?: Employee):
         if (!flUpdate) {
             throw error;
         }
-        throw responseText ? responseText : "Server is unavailable. Repeat later on";
+        throw responseText ? responseText : 'Server is unavailable. Repeat later on';
     }
 }
-async function fetchAllEmployees(url: string):Promise< Employee[]|string> {
+async function fetchAllEmployees(url: string): Promise<Employee[] | string> {
     const response = await fetchRequest(url, {});
-    return await response.json()
+    return await response.json();
 }
 
 export default class EmployeesServiceRest implements EmployeesService {
     private observable: Observable<Employee[] | string> | null = null;
-    private cache: Cache = new Cache();
-    private subscriber: Subscriber<string|Employee[]> | undefined;
-    constructor(private url: string) { }
+    private subscriber: Subscriber<string | Employee[]> | undefined;
+    private urlService: string;
+    private urlWebsocket: string;
+    private stompClient: CompatClient;
+    constructor(baseUrl: string) {
+        this.urlService = `http://${baseUrl}/employees`;
+        this.urlWebsocket = `ws://${baseUrl}/websocket/employees`;
+        this.stompClient = Stomp.client(this.urlWebsocket);
+    }
     async updateEmployee(empl: Employee): Promise<Employee> {
-        const response = await fetchRequest(this.getUrlWithId(empl.id!),
-            { method: 'PUT' }, empl);
-            this.sibscriberNext(this.url, this.subscriber!);
+        const response = await fetchRequest(this.getUrlWithId(empl.id!), { method: 'PUT' }, empl);
         return await response.json();
-
     }
     private getUrlWithId(id: any): string {
-        return `${this.url}/${id}`;
+        return `${this.urlService}/${id}`;
     }
-    private sibscriberNext(url: string, subscriber: Subscriber<Employee[] | string>): void {
-        
-        fetchAllEmployees(url).then(employees => {
-            if (this.cache.isEmpty() || !this.cache.isEqual(employees as Employee[])) {
-                this.cache.set(employees as Employee[]);
-                subscriber.next(employees);
-            }
-            
-        })
-        .catch(error => subscriber.next(error));
+    private sibscriberNext(): void {
+        fetchAllEmployees(this.urlService)
+            .then((employees) => {
+                this.subscriber?.next(employees);
+            })
+            .catch((error) => this.subscriber?.next(error));
     }
+
     async deleteEmployee(id: any): Promise<void> {
-            const response = await fetchRequest(this.getUrlWithId(id), {
-                method: 'DELETE',
-            });
-            this.sibscriberNext(this.url, this.subscriber!);
-           
+        await fetchRequest(this.getUrlWithId(id), {
+            method: 'DELETE',
+        });
     }
     getEmployees(): Observable<Employee[] | string> {
-        let intervalId: any;
         if (!this.observable) {
-            this.observable = new Observable<Employee[] | string>(subscriber => {
-                this.cache.reset();
-
-                this.sibscriberNext(this.url, subscriber);
+            this.observable = new Observable<Employee[] | string>((subscriber) => {
                 this.subscriber = subscriber;
-                intervalId = setInterval(() => this.sibscriberNext(this.url, subscriber), POLLER_INTERVAL);
-                return () => clearInterval(intervalId)
-            })
+                this.sibscriberNext();
+                this.connectWS();
+                return () => this.disconnectWS();
+            });
         }
         return this.observable;
     }
-       
-    async addEmployee(empl: Employee): Promise<Employee> {
-       
-            const response = await fetchRequest(this.url, {
-                method: 'POST',
-               }, empl)
-           ;
-           return response.json();
-
+    private disconnectWS(): void {
+        this.stompClient?.disconnect();
+    }
+    private connectWS() {        
+        this.stompClient.connect(
+            {},
+            () => {
+                this.stompClient?.subscribe(TOPIC, (message) => {
+                    console.log(message.body);
+                    this.sibscriberNext();
+                });
+            },
+            (error: any) => this.subscriber?.next(JSON.stringify(error)),
+            () => console.log('websocket disconected'),
+        );
     }
 
+    async addEmployee(empl: Employee): Promise<Employee> {
+        const response = await fetchRequest(
+            this.urlService,
+            {
+                method: 'POST',
+            },
+            empl,
+        );
+        return response.json();
+    }
 }
