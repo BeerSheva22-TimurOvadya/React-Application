316669fe18116be77385719e5d2e3b3d71a642fc
Ovadya33 <timurgts2710@gmail.com>
2023-09-19 14:09:27 +0300

    123

--- src/service/EmployeesServiceRest.ts
+++ src/service/EmployeesServiceRest.ts
@@ -110,14 +110,16 @@ export default class EmployeesServiceRest implements EmployeesService {
 
     }
        
-    async addEmployee(empl: Employee): Promise<Employee> {
-       
-            const response = await fetchRequest(this.urlService, {
-                method: 'POST',
-               }, empl)
-           ;
-           return response.json();
-
+    async addEmployee(empl: Employee): Promise<Employee> { 
+        if (empl.id == 0) { 
+            delete empl.id;     
+        } 
+            const response = await fetchRequest(this.urlService, { 
+                method: 'POST', 
+               }, empl) 
+           ; 
+           return response.json(); 
+ 
     }
 
 }
